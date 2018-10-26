f239538bc8d64e62c9459373c3d891992a1fb87c
Adam Lukasiewicz <adamlukasiewicz@ip-10-10-40-119.eu-central-1.compute.internal>
2018-10-26 09:58:02 +0200

    mirage setup

--- app/routes/gallery/neugeborene-babybauch.js
+++ app/routes/gallery/neugeborene-babybauch.js
@@ -2,41 +2,6 @@ import Route from '@ember/routing/route';
 
 export default Route.extend({
   model() {
-    return [{
-      "src": "http://martawilkphotography.com/img/newborn/medium/56.jpg",
-      "ratio": 0.66666666667
-    },
-    {
-      "src": "http://martawilkphotography.com/img/newborn/medium/55.jpg",
-      "ratio": 0.66666666667
-    },
-    {
-      "src": "http://martawilkphotography.com/img/newborn/medium/54.jpg",
-      "ratio": 1.5
-    },
-    {
-      "src": "http://martawilkphotography.com/img/newborn/medium/53.jpg",
-      "ratio": 0.66666666667
-    },
-    {
-      "src": "http://martawilkphotography.com/img/newborn/medium/52.jpg",
-      "ratio": 1.5
-    },
-    {
-      "src": "http://martawilkphotography.com/img/newborn/medium/51.jpg",
-      "ratio": 0.66666666667
-    },
-    {
-      "src": "http://martawilkphotography.com/img/newborn/medium/50.jpg",
-      "ratio": 1.5
-    },
-    {
-      "src": "http://martawilkphotography.com/img/newborn/medium/49.jpg",
-      "ratio": 0.66666666667
-    },
-    {
-      "src": "http://martawilkphotography.com/img/newborn/medium/48.jpg",
-      "ratio": 0.66666666667
-    }];
+    return this.store.findAll('photos');
   }
 });
--- mirage/config.js
+++ mirage/config.js
@@ -1,4 +1,46 @@
 export default function() {
+  this.namespace = '/api';
+
+  this.get('/photos', () => {
+    return {
+      data: [{
+        "src": "http://martawilkphotography.com/img/newborn/medium/56.jpg",
+        "ratio": 0.66666666667
+      },
+      {
+        "src": "http://martawilkphotography.com/img/newborn/medium/55.jpg",
+        "ratio": 0.66666666667
+      },
+      {
+        "src": "http://martawilkphotography.com/img/newborn/medium/54.jpg",
+        "ratio": 1.5
+      },
+      {
+        "src": "http://martawilkphotography.com/img/newborn/medium/53.jpg",
+        "ratio": 0.66666666667
+      },
+      {
+        "src": "http://martawilkphotography.com/img/newborn/medium/52.jpg",
+        "ratio": 1.5
+      },
+      {
+        "src": "http://martawilkphotography.com/img/newborn/medium/51.jpg",
+        "ratio": 0.66666666667
+      },
+      {
+        "src": "http://martawilkphotography.com/img/newborn/medium/50.jpg",
+        "ratio": 1.5
+      },
+      {
+        "src": "http://martawilkphotography.com/img/newborn/medium/49.jpg",
+        "ratio": 0.66666666667
+      },
+      {
+        "src": "http://martawilkphotography.com/img/newborn/medium/48.jpg",
+        "ratio": 0.66666666667
+      }]
+    };
+  });
 
   // These comments are here to help you get started. Feel free to delete them.
 
