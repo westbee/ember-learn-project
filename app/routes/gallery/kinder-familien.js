397dcd120e288abdb84f41b038a7968e10620a5e
Adam Lukasiewicz <adamlukasiewicz@ip-10-10-40-119.eu-central-1.compute.internal>
2018-10-29 13:05:46 +0100

    setting galleries using fixtures

--- app/routes/gallery/kinder-familien.js
+++ app/routes/gallery/kinder-familien.js
@@ -2,41 +2,6 @@ import Route from '@ember/routing/route';
 
 export default Route.extend({
   model() {
-    return [{
-      "src": "http://martawilkphotography.com/img/kinder/medium/70.jpg",
-      "ratio": 1.5
-    },
-    {
-      "src": "http://martawilkphotography.com/img/kinder/medium/69.jpg",
-      "ratio": 1.5
-    },
-    {
-      "src": "http://martawilkphotography.com/img/kinder/medium/68.jpg",
-      "ratio": 1.5
-    },
-    {
-      "src": "http://martawilkphotography.com/img/kinder/medium/67.jpg",
-      "ratio": 1.5
-    },
-    {
-      "src": "http://martawilkphotography.com/img/kinder/medium/66.jpg",
-      "ratio": 0.66666666667
-    },
-    {
-      "src": "http://martawilkphotography.com/img/kinder/medium/65.jpg",
-      "ratio": 1.5
-    },
-    {
-      "src": "http://martawilkphotography.com/img/kinder/medium/64.jpg",
-      "ratio": 1.5
-    },
-    {
-      "src": "http://martawilkphotography.com/img/kinder/medium/60.jpg",
-      "ratio": 1.5
-    },
-    {
-      "src": "http://martawilkphotography.com/img/kinder/medium/62.jpg",
-      "ratio": 0.66666666667
-    }];
+    return this.store.findAll('photo');
   }
 });
--- mirage/fixtures/photos.js
+++ mirage/fixtures/photos.js
@@ -34,5 +34,76 @@ export default [
   {
     "src": "http://martawilkphotography.com/img/newborn/medium/48.jpg",
     "ratio": 0.66666666667
+  },
+  {
+    "src": "http://martawilkphotography.com/img/kinder/medium/70.jpg",
+    "ratio": 1.5
+  },
+  {
+    "src": "http://martawilkphotography.com/img/kinder/medium/69.jpg",
+    "ratio": 1.5
+  },
+  {
+    "src": "http://martawilkphotography.com/img/kinder/medium/68.jpg",
+    "ratio": 1.5
+  },
+  {
+    "src": "http://martawilkphotography.com/img/kinder/medium/67.jpg",
+    "ratio": 1.5
+  },
+  {
+    "src": "http://martawilkphotography.com/img/kinder/medium/66.jpg",
+    "ratio": 0.66666666667
+  },
+  {
+    "src": "http://martawilkphotography.com/img/kinder/medium/65.jpg",
+    "ratio": 1.5
+  },
+  {
+    "src": "http://martawilkphotography.com/img/kinder/medium/64.jpg",
+    "ratio": 1.5
+  },
+  {
+    "src": "http://martawilkphotography.com/img/kinder/medium/60.jpg",
+    "ratio": 1.5
+  },
+  {
+    "src": "http://martawilkphotography.com/img/kinder/medium/62.jpg",
+    "ratio": 0.66666666667
+  },{
+    "src": "http://martawilkphotography.com/img/portrait/medium/44.jpg",
+    "ratio": 0.66666666667
+  },
+  {
+    "src": "http://martawilkphotography.com/img/portrait/medium/43.jpg",
+    "ratio": 0.66666666667
+  },
+  {
+    "src": "http://martawilkphotography.com/img/portrait/medium/42.jpg",
+    "ratio": 1.5
+  },
+  {
+    "src": "http://martawilkphotography.com/img/portrait/medium/41.jpg",
+    "ratio": 1.5
+  },
+  {
+    "src": "http://martawilkphotography.com/img/portrait/medium/40.jpg",
+    "ratio": 1.5
+  },
+  {
+    "src": "http://martawilkphotography.com/img/portrait/medium/39.jpg",
+    "ratio": 0.66666666667
+  },
+  {
+    "src": "http://martawilkphotography.com/img/portrait/medium/38.jpg",
+    "ratio": 0.66666666667
+  },
+  {
+    "src": "http://martawilkphotography.com/img/portrait/medium/37.jpg",
+    "ratio": 1.5
+  },
+  {
+    "src": "http://martawilkphotography.com/img/portrait/medium/36.jpg",
+    "ratio": 0.66666666667
   }
 ];
