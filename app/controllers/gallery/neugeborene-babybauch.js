112c47da5fad532e8d961ec740a81428beb27de4
Adam Lukasiewicz <adamlukasiewicz@ip-10-10-40-119.eu-central-1.compute.internal>
2018-10-29 12:01:41 +0100

    gallery neu set

---
+++ app/controllers/gallery/neugeborene-babybauch.js
@@ -0,0 +1,8 @@
+import Controller from '@ember/controller';
+import { map } from '@ember/object/computed';
+
+export default Controller.extend({
+  mappedModel: map('model', function(item) {
+    return item.getProperties('src', 'ratio');
+  })
+});
---
+++ mirage/fixtures/photos.js
@@ -0,0 +1,38 @@
+export default [
+  {
+    "src": "http://martawilkphotography.com/img/newborn/medium/56.jpg",
+    "ratio": 0.66666666667
+  },
+  {
+    "src": "http://martawilkphotography.com/img/newborn/medium/55.jpg",
+    "ratio": 0.66666666667
+  },
+  {
+    "src": "http://martawilkphotography.com/img/newborn/medium/54.jpg",
+    "ratio": 1.5
+  },
+  {
+    "src": "http://martawilkphotography.com/img/newborn/medium/53.jpg",
+    "ratio": 0.66666666667
+  },
+  {
+    "src": "http://martawilkphotography.com/img/newborn/medium/52.jpg",
+    "ratio": 1.5
+  },
+  {
+    "src": "http://martawilkphotography.com/img/newborn/medium/51.jpg",
+    "ratio": 0.66666666667
+  },
+  {
+    "src": "http://martawilkphotography.com/img/newborn/medium/50.jpg",
+    "ratio": 1.5
+  },
+  {
+    "src": "http://martawilkphotography.com/img/newborn/medium/49.jpg",
+    "ratio": 0.66666666667
+  },
+  {
+    "src": "http://martawilkphotography.com/img/newborn/medium/48.jpg",
+    "ratio": 0.66666666667
+  }
+];
