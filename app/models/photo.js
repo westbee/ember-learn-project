3a07e2c72184d8c847e9be34ed5d2bb68f969a9a
Adam Lukasiewicz <adamlukasiewicz@ip-10-10-40-119.eu-central-1.compute.internal>
2018-11-01 12:24:57 +0100

    href attr for photos

--- app/models/photo.js
+++ app/models/photo.js
@@ -4,6 +4,7 @@ export default DS.Model.extend({
   src: DS.attr('string'),
   ratio: DS.attr('number'),
   categoryId: DS.attr('number'),
+  href: DS.attr('string'),
 
   gallery: DS.belongsTo('gallery-category')
 
