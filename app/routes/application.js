3f8ba78c5be7618cefdb6efe852f506e80da7677
Adam Lukasiewicz <adamlukasiewicz@ip-10-10-40-119.eu-central-1.compute.internal>
2018-11-06 10:35:25 +0100

    rebuild the category-list component with the gallery model

--- app/routes/application.js
+++ app/routes/application.js
@@ -2,23 +2,6 @@ import Route from '@ember/routing/route';
 
 export default Route.extend({
   model() {
-    return [{
-    name: "Neugeborene & Babybauch",
-    title: "- die schönste Zeit im Leben einer Frau -",
-    pic: "http://martawilkphotography.com/img/cat1.jpg",
-    link: "gallery/neugeborene-babybauch"
-  },
-  {
-    name: "Kinder & Familien",
-    title: "- einzigartige Andenken für viele Jahre -",
-    pic: "http://martawilkphotography.com/img/cat2.jpg",
-    link: "gallery/kinder-familien"
-  },
-  {
-    name: "Porträtfotografie",
-    title: "- eine kurze Geschichte über Dich -",
-    pic: "http://martawilkphotography.com/img/top/top%206.jpg",
-    link: "gallery/portraitphotographie"
-  }];
+    return this.store.findAll('gallery');
   }
 });
