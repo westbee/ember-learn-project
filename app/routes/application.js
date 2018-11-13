af2a1ce5d24e616197fef7b236e95936ad5b8062
Adam Lukasiewicz <adamlukasiewicz@ip-10-10-40-119.eu-central-1.compute.internal>
2018-11-13 13:11:04 +0100

    create multiple models

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,7 +1,14 @@
 import Route from '@ember/routing/route';
+import RSVP from 'rsvp';
 
 export default Route.extend({
+  // model() {
+  //   return this.store.findAll('gallery');
+  // }
   model() {
-    return this.store.findAll('gallery');
+    return RSVP.hash({
+      galleries: this.store.findAll('gallery'),
+      photos: this.store.findAll('photo')
+    });
   }
 });
