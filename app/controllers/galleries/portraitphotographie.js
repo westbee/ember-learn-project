debcb6b71b34ece0437e5fd48398c3e0bcd34b66
Adam Lukasiewicz <adamlukasiewicz@ip-10-10-40-119.eu-central-1.compute.internal>
2018-11-14 10:00:41 +0100

    change many controllers into one

--- app/controllers/galleries/portraitphotographie.js
+++
@@ -1,8 +0,0 @@
-import Controller from '@ember/controller';
-import { map } from '@ember/object/computed';
-
-export default Controller.extend({
-  mappedModel: map('model', function(item) {
-    return item.getProperties('src', 'ratio', 'categoryId', 'href');
-  })
-});
