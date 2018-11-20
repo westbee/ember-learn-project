c8c2f4d2364178bd060689df3df71a5ae3e755e6
Adam Lukasiewicz <adamlukasiewicz@ip-10-10-40-119.eu-central-1.compute.internal>
2018-11-20 12:34:57 +0100

    delete old gallery routes

--- app/routes/galleries/kinder-familien.js
+++
@@ -1,9 +0,0 @@
-import Route from '@ember/routing/route';
-
-export default Route.extend({
-  model() {
-    return this.store.findAll('photo').then(function(kinder) {
-      return kinder.filterBy('categoryId', 2);
-    });
-  }
-});
--- app/routes/galleries/portraitphotographie.js
+++
@@ -1,9 +0,0 @@
-import Route from '@ember/routing/route';
-
-export default Route.extend({
-  model() {
-    return this.store.findAll('photo').then(function(portrait) {
-      return portrait.filterBy('categoryId', 3);
-    });
-  }
-});
