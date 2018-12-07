747f81b4985032437b0c528a41cbaef8d72332d9
Adam Lukasiewicz <mr.westbee@gmail.com>
2018-12-07 11:20:24 +0100

    remove map method for displaying photos in galleries

--- app/controllers/galleries/gallery.js
+++ app/controllers/galleries/gallery.js
@@ -1,11 +1,13 @@
 import Controller from '@ember/controller';
 import { map } from '@ember/object/computed';
 
+import { computed } from '@ember/object';
 export default Controller.extend({
-  // mappedPhotos: map('model.photos', function(item) {
-  //   return item.getProperties('src', 'ratio', 'galleryId', 'href');
-  // })
-  mappedPhotos: map('model.photos', function(photos) {
-    return photos;
+  // mappedPhotos: map('model.photos', function(photo) {
+  //   return photo;
+  // }),
+  // The plugin cannot understand EmberData hasMany relationships and requires an actual array
+  modelArray: computed('model.photos', function() {
+    return this.get('model.photos').toArray();
   })
 });
--- app/routes/galleries/gallery.js
+++ app/routes/galleries/gallery.js
@@ -3,5 +3,9 @@ import Route from '@ember/routing/route';
 export default Route.extend({
   model(params) {
     return this.store.findRecord('gallery', params.gallery_id);
+  },
+  async afterModel(model) {
+    await model.get('photos');
+    return this._super(...arguments);
   }
 });
