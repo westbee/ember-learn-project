3eb90643283199de7e4f99999b72fde4d9de93c2
Adam Lukasiewicz <mr.westbee@gmail.com>
2018-12-11 12:44:57 +0100

    style the ember-collapsible-panel

--- mirage/fixtures/infos.js
+++ mirage/fixtures/infos.js
@@ -1,14 +1,14 @@
 export default [
   {
-    question: 'EINIGE INFORMATIONEN ZUM ABLAUF',
+    question: '> EINIGE INFORMATIONEN ZUM ABLAUF <',
     answer: 'Wir besprechen im Vorfeld Eure Wünsche und Vorstellungen und Ihr entscheidet Euch, welches Paket Ihr buchen möchtet. Im Anschluss an das Fotoshooting wird jedes Bild von mir individuell und professionell bearbeitet. Die Bilder werden dann in einer persönlichen Online-Galerie bereitgestellt und gedruckt im Format 13x19.'
   },
   {
-    question: 'WELCHE PAKETE BIETE ICH AN?',
+    question: '> WELCHE PAKETE BIETE ICH AN? <',
     answer: 'Schreibt mir einfach eine e-mail oder ruft mich an und ich stelle Euch ein individuelles Angebot zusammen.'
   },
   {
-    question: 'DER GUTSCHEIN',
+    question: '> DER GUTSCHEIN <',
     answer: 'Sucht Ihr ein ganz besonderes und persönliches Geschenk für Eure Familie oder Freunde? Ein Gutschein für ein individuelles Fotoshooting in Berlin ist für jeden genau die richtige Geschenkidee!'
   }
 ]
